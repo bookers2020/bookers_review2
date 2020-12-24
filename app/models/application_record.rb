e0a857deda55863e479bafebc8063321a88c5018
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-12-24 14:45:54 +0900

    search-add

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,17 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+    def self.search(method, word)
+      if method == "forward_match"
+        @books = Book.where("title LIKE ?", "#{word}%")
+      elsif method == "backward_match"
+        @books = Book.where("title LIKE ?", "%#{word}")
+      elsif method == "perfect_match"
+        @books = Book.where(title: word)
+      elsif method == "partial_match"
+        @books = Book.where("title LIKE ?", "%#{word}%")
+      else
+        @books = Book.all
+      end
+  	end
 end
